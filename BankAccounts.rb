b6c9586f08aeb1cb9cda6b1e8dd8c354dd5c517b
Jade Vance <jadenicolevance@gmail.com>
2016-06-28 16:09:18 -0700

    added more hints

--- BankAccounts.rb
+++ BankAccounts.rb
@@ -1,6 +1,14 @@
+# Another hint :)
+require 'CSV'
+
 module Bank
   class Account
     # read up on attr_reader, attr_writer, attr_accessor
     # read up on class variables (@variable)
+    # The general pattern: 
+      # def initianlize(stuff_comes_in)
+          # @stuff = stuff_comes_in
+      # end
+      # now if you want to access stuff, you can just use @stuff rather than passing a local variable around
   end 
 end
